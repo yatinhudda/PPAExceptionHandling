97bb1584cc3ddb2705184af82c99b864f95bf98c
yatinhudda <yatinchaudhary72@gmail.com>
2023-09-09 22:28:12 +0530

    Java own exception

---
+++ src/main/java/IfNotExceptionHandling/Tester.java
@@ -0,0 +1,15 @@
+package IfNotExceptionHandling;
+
+public class Tester {
+    public static void main(String[] args){
+        int x = 30;
+        int y = 70;
+        Tester.callMe();
+    }
+    static void callMe(){
+        int y = 70;
+        int x = 0;
+        int z = y/x;
+        System.out.println(z);
+    }
+}
